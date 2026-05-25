b5b0fa3f103cd4623a8c50bc46b5a82e40ab93cf
NVTrungHuceIT <1018370@st.huce.edu.vn>
2026-05-25 23:24:07 +0700

    suafile lan cuoi

--- adtqueue/regular/queuelinked.cpp
+++ adtqueue/regular/queuelinked.cpp
@@ -5,4 +5,31 @@ using namespace std;
 struct node {
     int data;
     node * next;
+};
+
+void enqueue (node *front, node *rear, int x ) {
+    node *p = new node;
+    p->data = x;
+    p->data = NULL;
+    if (rear == NULL) {
+        front = rear = p;
+    }
+    else {
+        rear ->next = p; 
+        rear = p;
+    }
+}
+
+bool dequeue (node *front, node *rear, int x) {
+    if (front == NULL) return false;
+
+    node *temp = front;
+    x = temp->data;
+    front  = front ->next;
+    if (front  == NULL) {
+        rear = NULL;
+    }
+
+    delete temp;
+    return true;
 }
